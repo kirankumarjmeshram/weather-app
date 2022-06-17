5c6396f43e4b4a681baca87de899f41c0ff00bb9
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-06-17 16:20:27 +0530

    app is working

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import './App.css'
+import React, { useState } from 'react'
+import axios from 'axios'
+
+
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=e9701339369ffbc20c7c70edf1cabadc`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+  //showPosition
+//   function showPosition() {
+//     if(navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(function(position) {
+//             var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
+//             document.getElementById("result").innerHTML = positionInfo;
+//         });
+//     } else {
+//         alert("Sorry, your browser does not support HTML5 geolocation.");
+//     }
+// }
+
+
+  return (
+    <div className="App">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+      <div>
+        <button type="button" onclick="showPosition();">
+          Show Position
+        </button>
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{Math.round((data.main.temp.toFixed()-32)*5/9)}°C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
